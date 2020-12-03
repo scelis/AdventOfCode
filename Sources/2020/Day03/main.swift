b9e21c2bc873a7f2b6e28a294871eeab53e27cad
Sebastian Celis <sebastian@sebastiancelis.com>
2020-12-03 08:01:36 -0600

    Add solution for 2020-03

--- Sources/2020/Day03/main.swift
+++ Sources/2020/Day03/main.swift
@@ -3,11 +3,37 @@ import Foundation
 
 class Day03: Day {
     override func part1() -> String {
-        return ""
+        return numTrees(onSlope: Coordinate2D(x: 3, y: 1)).description
     }
 
     override func part2() -> String {
-        return ""
+        let slopes = [
+            Coordinate2D(x: 1, y: 1),
+            Coordinate2D(x: 3, y: 1),
+            Coordinate2D(x: 5, y: 1),
+            Coordinate2D(x: 7, y: 1),
+            Coordinate2D(x: 1, y: 2)
+        ]
+        return slopes
+            .map({ numTrees(onSlope: $0) })
+            .reduce(1, *)
+            .description
+    }
+
+    func numTrees(onSlope slope: Coordinate2D<Int>) -> Int {
+        var numTrees = 0
+        let xLength = inputLines[0].count
+        var current = Coordinate2D(x: 0, y: 0)
+        while current.y < inputLines.count {
+            if inputLines[current.y][current.x] == "#" {
+                numTrees += 1
+            }
+            current = Coordinate2D(
+                x: (current.x + slope.x) % xLength,
+                y: current.y + slope.y
+            )
+        }
+        return numTrees
     }
 }
 
