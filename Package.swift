68bab90940e42ce7cc22364c604990615ce1c0a4
Sebastian Celis <sebastian@sebastiancelis.com>
2020-12-03 11:38:39 -0600

    Add old solution to 2019-13

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .target(name: "AOC-2019-10", dependencies: ["AdventKit"], path: "Sources/2019/Day10", resources: [.copy("input.txt")]),
         .target(name: "AOC-2019-11", dependencies: ["AdventKit", "IntcodeComputer"], path: "Sources/2019/Day11", resources: [.copy("input.txt")]),
         .target(name: "AOC-2019-12", dependencies: ["AdventKit"], path: "Sources/2019/Day12", resources: [.copy("input.txt")]),
-        .target(name: "AOC-2019-13", dependencies: ["AdventKit"], path: "Sources/2019/Day13", resources: [.copy("input.txt")]),
+        .target(name: "AOC-2019-13", dependencies: ["AdventKit", "IntcodeComputer"], path: "Sources/2019/Day13", resources: [.copy("input.txt")]),
         .target(name: "AOC-2019-14", dependencies: ["AdventKit"], path: "Sources/2019/Day14", resources: [.copy("input.txt")]),
         .target(name: "AOC-2019-15", dependencies: ["AdventKit"], path: "Sources/2019/Day15", resources: [.copy("input.txt")]),
         .target(name: "AOC-2019-16", dependencies: ["AdventKit"], path: "Sources/2019/Day16", resources: [.copy("input.txt")]),
