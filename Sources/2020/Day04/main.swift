0d23ba9895471d62bdb65dd987345cbfa76cf87e
Sebastian Celis <sebastian@sebastiancelis.com>
2020-12-04 08:26:02 -0600

    Simplify the isValid function

--- Sources/2020/Day04/main.swift
+++ Sources/2020/Day04/main.swift
@@ -13,40 +13,35 @@ class Day04: Day {
         case countryID = "cid"
 
         func isValid(value: String?, strict: Bool) -> Bool {
-            if let value = value {
-                if strict {
-                    switch self {
-                    case .countryID:
-                        return true
-                    case .birthYear:
-                        return (1920...2002).contains(Int(value) ?? 0)
-                    case .issueYear:
-                        return (2010...2020).contains(Int(value) ?? 0)
-                    case .expirationYear:
-                        return (2020...2030).contains(Int(value) ?? 0)
-                    case .height:
-                        var value = value
-                        if value.hasSuffix("cm") {
-                            value.removeLast(2)
-                            return (150...193).contains(Int(value) ?? 0)
-                        } else if value.hasSuffix("in") {
-                            value.removeLast(2)
-                            return (59...76).contains(Int(value) ?? 0)
-                        } else {
-                            return false
-                        }
-                    case .hairColor:
-                        return value.range(of: "^#[0-9a-f]{6}$", options: .regularExpression) != nil
-                    case .eyeColor:
-                        return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(value)
-                    case .passportID:
-                        return value.range(of: "^[0-9]{9}$", options: .regularExpression) != nil
-                    }
+            guard let value = value else { return self == .countryID }
+            guard strict else { return true }
+
+            switch self {
+            case .countryID:
+                return true
+            case .birthYear:
+                return (1920...2002).contains(Int(value) ?? 0)
+            case .issueYear:
+                return (2010...2020).contains(Int(value) ?? 0)
+            case .expirationYear:
+                return (2020...2030).contains(Int(value) ?? 0)
+            case .height:
+                var value = value
+                if value.hasSuffix("cm") {
+                    value.removeLast(2)
+                    return (150...193).contains(Int(value) ?? 0)
+                } else if value.hasSuffix("in") {
+                    value.removeLast(2)
+                    return (59...76).contains(Int(value) ?? 0)
                 } else {
-                    return true
+                    return false
                 }
-            } else {
-                return self == .countryID
+            case .hairColor:
+                return value.range(of: "^#[0-9a-f]{6}$", options: .regularExpression) != nil
+            case .eyeColor:
+                return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(value)
+            case .passportID:
+                return value.range(of: "^[0-9]{9}$", options: .regularExpression) != nil
             }
         }
     }
