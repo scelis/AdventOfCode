766b0cc3c3a10185c7275f1a522d897a095399d7
Sebastian Celis <sebastian@sebastiancelis.com>
2020-12-06 09:51:39 -0600

    Add solution for day 6 of 2020

--- Sources/2020/Day06/main.swift
+++ Sources/2020/Day06/main.swift
@@ -3,11 +3,20 @@ import Foundation
 
 class Day06: Day {
     override func part1() -> String {
-        return ""
+        inputLines
+            .split(separator: "")
+            .map({ Set($0.joined()) })
+            .reduce(0, { $0 + $1.count })
+            .description
     }
 
     override func part2() -> String {
-        return ""
+        inputLines
+            .split(separator: "")
+            .map { arr in
+                arr.reduce(Set(arr.first!), { $0.intersection($1) })
+            }.reduce(0, { $0 + $1.count })
+            .description
     }
 }
 
