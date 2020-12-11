761b67e570c70dd45762e9bc08aecd42cf17fd65
Sebastian Celis <sebastian@sebastiancelis.com>
2020-12-11 10:52:58 -0600

    Solve 2020 day 11

--- Sources/2020/Day11/main.swift
+++ Sources/2020/Day11/main.swift
@@ -1,14 +1,101 @@
 import AdventKit
+import Algorithms
 import Foundation
 
 class Day11: Day {
+    enum Tile: Character {
+        case floor = "."
+        case emptySeat = "L"
+        case occupiedSeat = "#"
+    }
+
+    lazy var tiles: [Coordinate2D<Int>: Tile] = {
+        var dict = [Coordinate2D<Int>: Tile]()
+        for (y, line) in inputLines.enumerated() {
+            for (x, character) in line.enumerated() {
+                dict[Coordinate2D(x: x, y: y)] = Tile(rawValue: character)
+            }
+        }
+        return dict
+    }()
+
+    func solve(part: Int) -> Int {
+        var current = tiles
+
+        while true {
+            var next = current
+            var changed = false
+
+            for (coordinate, tile) in current {
+                if tile == .floor { continue }
+
+                var sum = 0
+                if part == 1 {
+                    sum = CardinalDirection.allDirections
+                        .map({ coordinate.step(inCardinalDirection: $0) })
+                        .compactMap({ current[$0] })
+                        .filter({ $0 == .occupiedSeat })
+                        .count
+                } else {
+                    for direction in CardinalDirection.allDirections {
+                        var steps = 1
+                        walking: while true {
+                            switch current[coordinate.step(inCardinalDirection: direction, distance: steps)] {
+                            case .emptySeat:
+                                break walking
+                            case .occupiedSeat:
+                                sum += 1
+                                break walking
+                            case .floor:
+                                steps += 1
+                                continue walking
+                            case nil:
+                                break walking
+                            }
+                        }
+                    }
+                }
+
+                if tile == .emptySeat && sum == 0 {
+                    next[coordinate] = .occupiedSeat
+                    changed = true
+                } else if tile == .occupiedSeat && sum >= (part == 1 ? 4 : 5) {
+                    next[coordinate] = .emptySeat
+                    changed = true
+                }
+            }
+
+            if !changed {
+                return current.values.filter({ $0 == .occupiedSeat }).count
+            } else {
+                current = next
+            }
+        }
+    }
+
     override func part1() -> String {
-        return ""
+        return solve(part: 1).description
     }
 
     override func part2() -> String {
-        return ""
+        return solve(part: 2).description
     }
 }
+
+let example1 = """
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL
+"""
+
+assert(Day11(input: example1).part1() == "37")
+assert(Day11(input: example1).part2() == "26")
 
 Day11().run()
--- Sources/AdventKit/CardinalDirection.swift
+++ Sources/AdventKit/CardinalDirection.swift
@@ -1,23 +1,31 @@
 import Foundation
 
-public enum CardinalDirection: String, CaseIterable {
+public enum CardinalDirection: String {
     case north = "N"
     case south = "S"
     case east = "E"
     case west = "W"
 
+    case northEast = "NE"
+    case northWest = "NW"
+    case southEast = "SE"
+    case southWest = "SW"
+
+    public static var mainDirections: [CardinalDirection] = [.north, .east, .south, .west]
+    public static var allDirections: [CardinalDirection] = [.northWest, .north, .northEast, .east, .southEast, .south, .southWest, .west]
+
     public var dx: Int {
         switch self {
         case .north, .south: return 0
-        case .west: return -1
-        case .east: return 1
+        case .west, .southWest, .northWest: return -1
+        case .east, .southEast, .northEast: return 1
         }
     }
 
     public var dy: Int {
         switch self {
-        case .north: return -1
-        case .south: return 1
+        case .north, .northEast, .northWest: return -1
+        case .south, .southEast, .southWest: return 1
         case .west, .east: return 0
         }
     }
@@ -25,7 +33,11 @@ public enum CardinalDirection: String, CaseIterable {
     public var opposite: CardinalDirection {
         switch self {
         case .north: return .south
+        case .northEast: return .southWest
+        case .northWest: return .southEast
         case .south: return .north
+        case .southEast: return .southWest
+        case .southWest: return .southEast
         case .east: return .west
         case .west: return .east
         }
