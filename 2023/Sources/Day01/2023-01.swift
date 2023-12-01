98086617e35eaae061d497dfde546f3f75b4185a
Sebastian Celis <sebastian@sebastiancelis.com>
2023-12-01 08:30:45 -0600

    Solve 2023 day 1

--- 2023/Sources/Day01/2023-01.swift
+++ 2023/Sources/Day01/2023-01.swift
@@ -2,11 +2,64 @@ import AdventKit
 import Foundation
 
 public class Day01: Day<Int, Int> {
+    let digits: [String: Int] = [
+        "0": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9
+    ]
+
+    let words: [String: Int] = [
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    ]
+
+    func findCalibrationComponent(in string: String, using map: [String: Int], fromBeginning: Bool) -> Int? {
+        var s: String.SubSequence = .init(string)
+        while !s.isEmpty {
+            for (key, value) in map {
+                if (fromBeginning && s.hasPrefix(key)) || (!fromBeginning && s.hasSuffix(key)) {
+                    return value
+                }
+            }
+            s = fromBeginning ? s.dropFirst() : s.dropLast()
+        }
+        return nil
+    }
+
+    func calibrationValue(for string: String, using map: [String: Int]) -> Int? {
+        guard
+            let left = findCalibrationComponent(in: string, using: map, fromBeginning: true),
+            let right = findCalibrationComponent(in: string, using: map, fromBeginning: false)
+        else { return nil }
+
+        return left * 10 + right
+    }
+
     public override func part1() throws -> Int {
-        return 0
+        return inputLines
+            .compactMap { calibrationValue(for: $0, using: digits) }
+            .reduce(0, +)
     }
 
     public override func part2() throws -> Int {
-        return 0
+        let map: [String: Int] = digits.merging(words, uniquingKeysWith: { a, _ in a })
+        return inputLines
+            .compactMap { calibrationValue(for: $0, using: map) }
+            .reduce(0, +)
     }
 }
--- 2023/Tests/AOC2023Tests.swift
+++ 2023/Tests/AOC2023Tests.swift
@@ -5,7 +5,7 @@ import XCTest
 class AOC2023Tests: XCTestCase {
     func testDay01() throws {
         let day = Day01()
-        XCTAssertEqual(try day.part1(), 0)
-        XCTAssertEqual(try day.part2(), 0)
+        XCTAssertEqual(try day.part1(), 54951)
+        XCTAssertEqual(try day.part2(), 55218)
     }
 }
