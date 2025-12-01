f53a56dd7f6ce57f30481f37678736a24b62ed51
Sebastian Celis <sebastian@sebastiancelis.com>
2025-12-01 08:33:25 -0600

    Solve 2025 day 1

--- 2025/Sources/Day01/2025-01.swift
+++ 2025/Sources/Day01/2025-01.swift
@@ -2,17 +2,47 @@ import AdventKit
 import Foundation
 
 struct Day01: Day {
+    enum Turn {
+        case left(Int)
+        case right(Int)
+    }
+
     func run() async throws -> (Int, Int) {
-        async let p1 = part1()
-        async let p2 = part2()
-        return try await (p1, p2)
+        var dial = 50
+        var part1 = 0
+        var part2 = 0
+
+        inputLines()
+            .map { parse(line: $0) }
+            .forEach { turn in
+                let initial = dial
+
+                switch turn {
+                case .left(let amount): dial -= amount
+                case .right(let amount): dial += amount
+                }
+
+                if dial == 0 {
+                    part2 += 1
+                } else if dial < 0 {
+                    part2 += abs(dial / 100)
+                    part2 += (initial == 0) ? 0 : 1
+                    dial = ((dial % 100) + 100) % 100
+                } else if dial >= 100 {
+                    part2 += (dial / 100)
+                    dial = dial % 100
+                }
+
+                if dial == 0 {
+                    part1 += 1
+                }
+            }
+
+        return (part1, part2)
     }
 
-    func part1() async throws -> Int {
-        return 0
-    }
-
-    func part2() async throws -> Int {
-        return 0
+    func parse(line: String) -> Turn {
+        let amount = Int(line.dropFirst())!
+        return line.hasPrefix("L") ? .left(amount) : .right(amount)
     }
 }
--- 2025/Tests/AOC2025Tests.swift
+++ 2025/Tests/AOC2025Tests.swift
@@ -6,7 +6,7 @@ import Testing
     @Test func day01() async throws {
         let day = Day01()
         let parts = try await day.run()
-        #expect(parts.0 == 0)
-        #expect(parts.1 == 0)
+        #expect(parts.0 == 1052)
+        #expect(parts.1 == 6295)
     }
 }
